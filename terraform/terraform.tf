b474a5fc09eeaa29a9add6ee2742ccfe8afd1565
Blxd-Z <ilyasszn15@gmail.com>
2026-02-10 03:10:22 +0100

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,12 @@
 terraform {
 
+  backend "s3"{
+    bucket = "remote-tfstate-blx"
+    key = "tf.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+  
   required_providers { #Proveedor a utilizar, en este caso AWS, para que se pueda comunicar con la API de AWS.
     aws = {
       source  = "hashicorp/aws" #Hostname simplifado del registro de registry.terraform.io/hashicorp/aws
@@ -8,3 +15,4 @@ terraform {
   }
   required_version = ">= 1.14" #version a usar de terraform
 }
+
